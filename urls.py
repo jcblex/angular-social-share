705a62ccbd60c37f18cb1e52f045b1ab4ffb0e99
jcblex <jacob@sayonetech.com>
2017-03-22 12:47:26 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 from rest_framework import routers
 
 urlpatterns = [
-    url(r'^share-redirect-url/(?P<slug>[-\w]+)/$', SocialShare.as_view(), name='rich_share_redirect'),
+    url(r'^share-redirect-url/(?P<slug>[-\w]+)/$', RichSocialShare.as_view(), name='rich_share_redirect'),
 ]
